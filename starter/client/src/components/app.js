bd41a9976ddc66572b444e418e4e9ec42ddcf2df
meganr <meganrm@gmail.com>
2018-02-18 21:04:59 -0800

    upading app

--- starter/client/src/components/app.js
+++ starter/client/src/components/app.js
@@ -1,6 +1,6 @@
-import '../style/main.scss';
-
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import { Switch, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -14,26 +14,34 @@ import Profile from './profile';
 import * as authActions from '../app/actions/auth';
 import * as routeActions from '../app/actions/routes';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+import '../style/main.scss';
 
+class App extends React.Component {
   componentWillMount() {
     this.props.validate();
   }
 
   render() {
+    const {
+      auth,
+      logout,
+      route,
+      switchRoute,
+    } = this.props;
     return (
       <React.Fragment>
 
         <Header appTitle="Chat App" />
 
-        { /* TODO: Probably should send the routing actions and the route state so you can show/hide links in the menu */ }
-        <Navbar />
+        <Navbar auth={auth} switchRoute={switchRoute} handleLogout={logout} />
 
         <main>
-          { /* TODO: Insert Switch logic in here for routing */ }
+
+          <Switch location={{ pathname: route }}>
+            <Route exact path="/chat" component={Chat} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
         </main>
 
         <Footer>
@@ -51,10 +59,18 @@ const mapStateToProps = state => ({
   route: state.route,
 });
 
-const mapDispatchToProps = (dispatch, getState) => ({
+const mapDispatchToProps = dispatch => ({
   validate: () => dispatch(authActions.validate()),
   logout: () => dispatch(authActions.logout()),
   switchRoute: route => dispatch(routeActions.switchRoute(route)),
 });
+
+App.propTypes = {
+  auth: PropTypes.shape({}).isRequired,
+  logout: PropTypes.func.isRequired,
+  route: PropTypes.func.isRequired,
+  switchRoute: PropTypes.func.isRequired,
+  validate: PropTypes.func.isRequired,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
