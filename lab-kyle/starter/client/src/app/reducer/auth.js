dba44a3f64dbb5b9b8e94fcc1a78a581b8034084
Kyle Richey <richey.kyle.i@gmail.com>
2018-02-22 16:44:55 -0800

    all ToDos done Lab done

--- lab-kyle/starter/client/src/app/reducer/auth.js
+++ lab-kyle/starter/client/src/app/reducer/auth.js
@@ -1,7 +1,16 @@
 // TODO: Handle LOGIN & LOGOUT
 
-export default (state=null, action) => {
-    
+export default (state=false, action) => {
+
+  let {type, payload} = action;
+
+  switch(type) {
+    case 'LOGIN':
+    return true;
+
+    case 'LOGOUT':
+    return false;
+
+    default:
     return state;
-    
 }
--- lab-kyle/starter/client/src/components/navbar.js
+++ lab-kyle/starter/client/src/components/navbar.js
@@ -1,21 +1,35 @@
 import React from 'react';
+import {renderIf} from '../lib/__';
 
 class Navbar extends React.Component {
-    
-    render() {
-        
-        return (
+  constructor(props){
+    super(props);
 
-            <nav>
-                <ul>
-                   { /* TODO:  Need some dynamic links here for Logout, Profile, Chat to display only when you are logged in */ }
-                </ul>
-            </nav>
+    this.goToChat = this.goToChat.bind(this);
+  }
 
-        )
+  goToChat(){
+    this.props.switchRoute('/chat');
+  }
 
-    }
-
+  render() {
+    return (
+      <nav>
+        <ul>
+          { /* TODO:  Need some dynamic links here for Logout, Profile, Chat to display only when you are logged in */ }
+          {
+            renderIf(this.props.auth,
+              <React.Fragment>
+                <li><button onClick={this.goToChat}>Chat</button></li>
+                <li><button onClick={() => this.props.switchRoute('/profile')}>Profile</button></li>
+                <li><button onClick={this.props.handleLogout}>Logout</button></li>
+              </React.Fragment>
+            )
+          }
+        </ul>
+      /nav>
+    )
+  }
 }
 
 export default Navbar;
